cf391056eb75b3227747863e207ba18189426a81
Nathaniel Paff <NatPaff@Nathaniels-MacBook-Pro.local>
2021-04-01 20:39:14 +1300

    partty

--- src/Patient.java
+++ src/Patient.java
@@ -3,11 +3,22 @@ import java.util.List;
 import java.util.Map;
 
 public class Patient {
+    public String getPatientClass() {
+        return patientClass;
+    }
+
+    public String patientClass;
+
+    public Map<String, Boolean> getStats() {
+        return stats;
+    }
+
     private Map<String, Boolean> stats = new HashMap<>();
 
     public Patient(List<String> attribute, String patientData) {
         String[] data = patientData.split(" ");
-        for (int i = 0; i < data.length; i++) {
+        this.patientClass = data[0];
+        for (int i = 1; i < data.length; i++) {
             this.stats.put(attribute.get(i), Boolean.parseBoolean(data[i]));
         }
     }
